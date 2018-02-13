bfc7b3c4ecb1000e1b51cb98b8f0603903d60102
PETER CUTLER <PETERGCUTLER@GMAIL.COM>
2018-02-13 00:02:14 -0800

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 // import { CommonModule } from '@angular/common';
-// import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
 
 
