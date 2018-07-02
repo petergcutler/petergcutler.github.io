785f718db2dcbe023eebc4d757e7dc2b20bcc1fc
peter on windows <petergcutler@gmail.com>
2018-07-02 15:43:15 -0700

    update scroll top

--- src/app/article-footer/article-footer.component.ts
+++ src/app/article-footer/article-footer.component.ts
@@ -1,17 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-article-footer',
   templateUrl: './article-footer.component.html',
   styleUrls: ['./article-footer.component.scss']
 })
+
 export class ArticleFooterComponent implements OnInit {
   @Input()
   imageCredit: object;
 
-  constructor() { }
+  public longPage : boolean;
 
-  ngOnInit() {
+  scrollToTop(): void {
+    window.scroll({
+      top: 0,
+      behavior: "smooth"
+    });
   }
 
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  checkRoute(): void {
+    const url = this.route.snapshot.routeConfig.path;
+
+    if (url === 'work/details') {
+      this.longPage = true;
+    } else {
+      this.longPage = false;
+    }
+  }
+
+  ngOnInit(): void {
+    this.checkRoute();
+  }
 }
