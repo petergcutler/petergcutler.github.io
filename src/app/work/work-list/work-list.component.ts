276450d323034e9b1daa7ea9e359b28d49b8c171
peter on windows <petergcutler@gmail.com>
2018-07-20 17:39:52 -0700

    update a11y support and cleanup

--- src/app/work/work-list/work-list.component.ts
+++ src/app/work/work-list/work-list.component.ts
@@ -12,17 +12,6 @@ import { WorkService } from '../work.service';
 export class WorkListComponent implements OnInit {
 
   public works: Work[];
-  public colors = [
-    '#BF9A00', // light greenish blue green
-    '#f5cd79', // summertime yellow
-    '#74b9ff', // green darner tail blue
-    '#a29bfe', // shy moment purple
-    '#fab1a0', // first date pink
-    '#ff7675', // pink glamour pink
-    '#fd79a8', // piko eight pink
-    '#f3a683', // creamy peach pink
-    '#63cdda', // squeaky blue
-  ];
 
   constructor(private workService: WorkService) {}
 
