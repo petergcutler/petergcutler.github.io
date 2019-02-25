b37346691acd8bf6105fcaecac396d12fd9bd818
Peter Cutler <petercutler@Peters-MacBook-Pro.local>
2019-02-24 22:02:56 -0800

    add shuffling and polling and icons

--- src/app/mosaic/mosaic-display/mosaic-display.component.ts
+++ src/app/mosaic/mosaic-display/mosaic-display.component.ts
@@ -15,17 +15,27 @@ export class MosaicDisplayComponent implements OnInit {
   private sheetsReadUrl = 'https://spreadsheets.google.com/feeds/cells/1xGp8gww59dEO7SDvr8Ww6nRcvxFmx28A-_jPSxsdmNc/od6/public/basic?alt=json';
 
   public messages: any = [];
+  public messagesDisplay: any = [];
+
+  change(this) {
+    let that = this;
+
+    setInterval(function() {
+      that.messages.push(0);
+    }, 30000);
+  };
 
   buildMessages(data) {
 
     let that = this;
     let message = {
+      'id': null,
       'messageType': null,
       'messageContent': null
     };
     let messages = [];
 
-    _.each(data.feed.entry, function(i) {
+    _.each(data.feed.entry, function(i, n) {
       if (
         i.content["$t"] === 'thinking' ||
         i.content["$t"] === 'feeling' ||
@@ -34,23 +44,31 @@ export class MosaicDisplayComponent implements OnInit {
       ) {
         message.messageType = i.content["$t"];
       } else {
+        var order: number = +n + 1;
         message.messageContent = i.content["$t"]
+        message.id =  order / 2;
         messages.push(message);
         message = {
+          'id': null,
           'messageType': null,
           'messageContent': null
         }
       }
     });
 
-    if (messages != that.messages) {
+    if (!_.isEqual(messages, that.messages)) {
       that.messages = messages;
+      that.messagesDisplay = _.shuffle( _.clone(messages) );
+      _.pull(that.messages, 0);
     }
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
+    this.change();
+
     interval(5000)
       .pipe(
         startWith(0),
