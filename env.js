16dc26a5dca3adb013634c1bc02a2c252e448b0e
Peter Cutler <petercutler@Peters-MacBook-Pro.local>
2019-03-02 21:00:17 -0800

    Auto-generated commit

---
+++ env.js
@@ -0,0 +1,11 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  // API url
+  window.__env.AT = 'Bearer keyh4Qu4o8O9tbqij';
+  window.__env.ATU = 'https://api.airtable.com/v0/appeu3W1CT3mOA2t4/Table%201?maxRecords=50&view=Grid%20view';
+
+  // Whether or not to enable debug mode
+  // Setting this to false will disable console output
+  window.__env.enableDebug = true;
+}(this));
