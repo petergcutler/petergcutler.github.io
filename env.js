ec6fa6e84dc7d66bb24abbdc4ffee5d73ca0bb9e
Peter Cutler <petercutler@Peters-MacBook-Pro.local>
2019-03-03 00:33:03 -0800

    Auto-generated commit

--- env.js
+++ env.js
@@ -3,7 +3,7 @@
 
   // API url
   window.__env.AT = 'Bearer keyh4Qu4o8O9tbqij';
-  window.__env.ATU = 'https://api.airtable.com/v0/appeu3W1CT3mOA2t4/Table%201?maxRecords=50&view=Grid%20view';
+  window.__env.ATU = 'https://api.airtable.com/v0/appeu3W1CT3mOA2t4/Table%201?maxRecords=16&view=Grid%20view&sort%5B0%5D%5Bfield%5D=created&sort%5B0%5D%5Bdirection%5D=desc';
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
