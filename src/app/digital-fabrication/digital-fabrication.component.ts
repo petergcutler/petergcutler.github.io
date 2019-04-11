824fba6c8df7511818fd9b4c93c04d3a549dd84d
Peter Cutler <petercutler@Peters-MacBook-Pro.local>
2019-04-10 17:28:40 -0700

    updated digi fabric

--- src/app/digital-fabrication/digital-fabrication.component.ts
+++ src/app/digital-fabrication/digital-fabrication.component.ts
@@ -1,14 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-digital-fabrication',
   templateUrl: './digital-fabrication.component.html',
-  styleUrls: ['./digital-fabrication.component.scss']
+  styleUrls: ['./digital-fabrication.component.scss'],
+  animations: [
+    trigger('revealAboutItems', [
+      transition('loading => loaded', [
+        query('.about-item', style({
+          opacity: 0,
+          transform: 'translateY(5px)'
+          // transform: 'translateX(-40px)'
+        })),
+        query('.about-item', stagger('70ms', [
+          animate('700ms cubic-bezier(0.6, 0.2, 0.1, 1)', style({
+            opacity: 1,
+            transform: 'translateY(0)'
+            // transform: 'translateX(0)'
+          }))
+        ]))
+      ])
+    ])
+  ]
 })
 
 export class DigitalFabricationComponent implements OnInit {
 
-  constructor() {}
+  constructor(private cdRef: ChangeDetectorRef ) {}
+
+  revealAbout: string = 'loading';
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.revealAbout = 'loaded';
+    this.cdRef.detectChanges();
+  }
+
 }
