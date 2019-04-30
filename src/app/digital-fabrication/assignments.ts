2c588ef2e688b1efa4ece95a575b5952c36c6b79
Peter Cutler <petercutler@D-10-19-92-140.dhcp4.washington.edu>
2019-04-29 20:18:12 -0700

    add draft content

--- src/app/digital-fabrication/assignments.ts
+++ src/app/digital-fabrication/assignments.ts
@@ -2,8 +2,8 @@ import { Assignment } from './assignment';
 
 export const ASSIGNMENTS: Assignment[] = [
   { id: 1, name: 'A1: Laser Cut' },
-  { id: 2, name: 'A2: Grasshopper Laser Cut'}
-  // { id: 3, name: 'Bombasto' },
+  { id: 2, name: 'A2: Grasshopper Laser Cut'},
+  { id: 3, name: 'A3: 3d Printed Lamp' },
   // { id: 4, name: 'Celeritas' },
   // { id: 5, name: 'Magneta' },
   // { id: 6, name: 'RubberMan' },
