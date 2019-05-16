02d0191aec2cb4200dca3a556f3172922cbe5c27
Peter Cutler <petercutler@Peters-MacBook-Pro.local>
2019-05-15 17:36:17 -0700

    all assignment 5 work

--- src/app/digital-fabrication/assignments.ts
+++ src/app/digital-fabrication/assignments.ts
@@ -5,7 +5,7 @@ export const ASSIGNMENTS: Assignment[] = [
   { id: 2, name: 'A2: Grasshopper Laser Cut'},
   { id: 3, name: 'A3: 3d Printed Lamp' },
   { id: 4, name: 'A4: Moving Parts' },
-  // { id: 5, name: 'Magneta' },
+  { id: 5, name: 'A5: Subtractive Fabrication' },
   // { id: 6, name: 'RubberMan' },
   // { id: 7, name: 'Dynama' },
   // { id: 8, name: 'Dr IQ' },
