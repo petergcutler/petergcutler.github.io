888af54a41a97b4516321c9c4a343e0dabde6bd0
Peter Cutler <petergcutler@gmail.com>
2019-05-29 17:29:37 -0700

    add a7

--- src/app/digital-fabrication/assignments.ts
+++ src/app/digital-fabrication/assignments.ts
@@ -7,7 +7,7 @@ export const ASSIGNMENTS: Assignment[] = [
   { id: 4, name: 'A4: Moving Parts' },
   { id: 5, name: 'A5: Subtractive Fabrication' },
   { id: 6, name: 'A6: Molding and Casting' },
-  // { id: 7, name: 'Dynama' },
+  { id: 7, name: 'A7: Final Project Proposal' },
   // { id: 8, name: 'Dr IQ' },
   // { id: 9, name: 'Magma' },
   // { id: 10, name: 'Tornado' }
